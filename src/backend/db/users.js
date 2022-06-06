260f1837a17752401d3efc080d04630b4c134fe1
neetukumari4858 <90403664+neetukumari4858@users.noreply.github.com>
2022-06-06 14:23:56 +0530

    refector:changes Profile user Link

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -13,7 +13,7 @@ export const users = [
     username: 'Neetu',
     password: 'neetu123',
     bio: 'I am fresher in web Developement',
-    link: 'https://adarshbalika.netlify.app/',
+    link: 'https://candid-paletas-8107fc.netlify.app/index.html',
     profile:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRODPqk7Q74WjEwsgk_HqwvLnrUrWDc0txOS4Xm7qrDkQbRLBSR306nPO9UUf-cZTlArKc&usqp=CAU',
     createdAt: 2022,
@@ -26,7 +26,7 @@ export const users = [
     username: 'Elden',
     password: 'Elden123',
     bio: 'I am fresher in web Developement',
-    link: 'https://adarshbalika.netlify.app/',
+    link: 'https://candid-paletas-8107fc.netlify.app/index.html',
     profile:
       'https://png.pngitem.com/pimgs/s/570-5706525_tiffanie-circle-headshot-2018-blond-hd-png-download.png',
     createdAt: 2022,
@@ -40,7 +40,7 @@ export const users = [
     username: 'Charless',
     password: 'Charless123',
     bio: 'I am fresher in web Developement',
-    link: 'https://adarshbalika.netlify.app/',
+    link: 'https://candid-paletas-8107fc.netlify.app/index.html',
     profile:
       'https://www.kindpng.com/picc/m/168-1684606_circle-headshot-girl-hd-png-download.png',
     createdAt: 2022,
@@ -53,7 +53,7 @@ export const users = [
     username: 'Johan',
     password: 'Charless123',
     bio: 'I am fresher in web Developement',
-    link: 'https://adarshbalika.netlify.app/',
+    link: 'https://candid-paletas-8107fc.netlify.app/index.html',
     profile:
       'https://thumbs.dreamstime.com/z/cute-young-boy-smiling-21381704.jpg',
     createdAt: 2022,
@@ -66,7 +66,7 @@ export const users = [
     username: 'Anurag',
     password: 'Charless123',
     bio: 'I am fresher in web Developement',
-    link: 'https://adarshbalika.netlify.app/',
+    link: 'https://candid-paletas-8107fc.netlify.app/index.html',
     profile:
       'https://media.istockphoto.com/photos/portrait-of-a-happy-latin-american-boy-smiling-picture-id1271410473?k=20&m=1271410473&s=170667a&w=0&h=Y-w3AL1AeAGyqC-D4gDDhUVRTyouBx3useTA_FYU5wA=',
     createdAt: 2022,
