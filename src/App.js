875576871bb429d4645228fb636df933cb8e9563
neetukumari4858 <90403664+neetukumari4858@users.noreply.github.com>
2022-06-26 10:28:14 +0530

    refector:minor changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import "./App.css";
-import { Routers } from "./Routers";
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer} from 'react-toastify';
+import './App.css'
+import { Routers } from './Routers'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 function App() {
   return (
     <>
-    <ToastContainer
-    position="top-right"
-    autoClose={5000}
-    hideProgressBar={false}
-    newestOnTop={false}
-    closeOnClick
-    rtl={false}
-    fontSize={2}
-    pauseOnFocusLoss
-    draggable
-    pauseOnHover
-  />
-  <ToastContainer />
-    <div className="App">
-      <Routers/>      
-    </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <ToastContainer />
+      <div className="App">
+        <Routers />
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
